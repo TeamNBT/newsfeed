17d631dcd74fdb9ea8b85a83e663e444ecc6db17
Yerin <dev.yerin@gmail.com>
2024-06-01 18:56:23 +0900

    fix: 플러그인 적용

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,12 +5,12 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'unused-imports'],
   rules: {
     'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
