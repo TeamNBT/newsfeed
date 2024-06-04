ff53febf9461a54924e169e617a5e82552171982
Seung Bin Lee <leesb5510@gmail.com>
2024-06-04 17:23:34 +0900

    style: 코드 리뷰 수정

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,51 +1,109 @@
 import { useState } from 'react';
 import styled from 'styled-components';
+import { ellipsisStyle } from '@/styles/utils';
 
 const Card = () => {
   const [isLiked, setIsLiked] = useState(false);
+
   return (
-    <CardBox>
-      <div className="-Card">
-        <div className="image-23"></div>
-        <header>
-          <div className="Typo-Group-HStack">
-            <span className="prm-text">Sqruce</span>
-            <div className="Rectangle-23"></div>
-            <span className="scd-text">mari</span>
-          </div>
-          <button onClick={() => setIsLiked((prev) => !prev)}>
-            <img
-              src={
-                isLiked
-                  ? 'src/assets/images/common/ic_general_like_fill.svg'
-                  : 'src/assets/images/common/ic_general_like.svg'
-              }
-            ></img>
-          </button>
-        </header>
-      </div>
-    </CardBox>
+    <StCard>
+      <StImgBox>
+        <StImg src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREDxwO6rYgn8oqacoaavErgjKZo9KG3PaPig&s" />
+      </StImgBox>
+      <StHeader>
+        <StTypoGroupHStack>
+          <StTitle>안녕하세요 안녕 안녕</StTitle>
+          <StDivideBar />
+          <StAuthor>mari @스파르타 코딩 클럽</StAuthor>
+        </StTypoGroupHStack>
+        <StLikeButton onClick={() => setIsLiked((prev) => !prev)}>
+          <StLikeImg
+            src={
+              isLiked
+                ? 'src/assets/images/common/ic_general_like_fill.svg'
+                : 'src/assets/images/common/ic_general_like.svg'
+            }
+          ></StLikeImg>
+        </StLikeButton>
+      </StHeader>
+    </StCard>
   );
 };
 
-export const Cards = styled.section`
-  display: flex;
-  flex-wrap: wrap;
+const StCards = styled.section`
+  display: grid;
   gap: 32px 16px;
+  grid-template-columns: 1fr 1fr;
 `;
 
-const CardBox = styled.div`
-  .-Card {
-    width: 520px;
-    height: 343.4px;
-    flex-grow: 0;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: stretch;
-    gap: 22px;
-    padding: 0;
-  }
+const StTypoGroupHStack = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  gap: 9px;
 `;
 
-export default Card;
+const StCard = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+`;
+
+const StTitle = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  line-height: 1.4;
+  color: #ffffff;
+  ${ellipsisStyle(1)};
+`;
+
+const StAuthor = styled.span`
+  font-size: 16px;
+  line-height: 1.4;
+  color: #999999;
+  flex-shrink: 0;
+  max-width: 150px;
+  ${ellipsisStyle(1)};
+`;
+
+const StImgBox = styled.div`
+  border-radius: 12px;
+  overflow: hidden;
+`;
+
+const StImg = styled.img`
+  width: 100%;
+  background-color: white;
+  aspect-ratio: 1 / 0.66;
+  object-fit: cover;
+  display: block;
+`;
+
+const StHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 22px;
+  align-items: center;
+`;
+
+const StLikeImg = styled.img`
+  width: 24px;
+  height: 24px;
+  flex-grow: 0;
+  object-fit: contain;
+`;
+
+const StLikeButton = styled.button`
+  width: 24px;
+  height: 24px;
+`;
+
+const StDivideBar = styled.div`
+  width: 1px;
+  height: 16px;
+  flex-grow: 0;
+  background-color: rgba(217, 217, 217, 0.8);
+`;
+
+export { Card, StCards };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,102 +1,23 @@
 import styled from 'styled-components';
-import SectionHeader from '@/pages/Home/SectionHeader';
-import Card, { Cards } from '@/components/Card/Card';
+import SectionHeaderHook from '@/pages/Home/SectionHeader';
+import { Card, StCards } from '@/components/Card';
 
 const Home = () => {
   return (
-    <div>
-      <Article>
-        <SectionHeader />
-        <Cards>
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-        </Cards>
-      </Article>
-    </div>
+    <StArticle>
+      <SectionHeaderHook />
+      <StCards>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </StCards>
+    </StArticle>
   );
 };
 
-const Article = styled.article`
-  height: 1177.3px;
-  gap: 16px;
-  padding: 0;
-  .Typo-Group-HStack {
-    height: 28px;
-    flex-grow: 1;
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
-    gap: 9px;
-    padding: 0;
-  }
-  .prm-text {
-    width: 70px;
-    height: 28px;
-    flex-grow: 0;
-    font-family: Pretendard;
-    font-size: 20px;
-    font-weight: 600;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: 1.4;
-    letter-spacing: normal;
-    text-align: left;
-    color: #fff;
-  }
-  .Rectangle-23 {
-    width: 1px;
-    height: 16px;
-    flex-grow: 0;
-    background-color: rgba(217, 217, 217, 0.8);
-  }
-  .scd-text {
-    width: 147px;
-    height: 22px;
-    flex-grow: 0;
-    font-family: Pretendard;
-    font-size: 16px;
-    font-weight: normal;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: 1.4;
-    letter-spacing: normal;
-    text-align: left;
-    color: #999;
-  }
-  .divider {
-    width: 1056px;
-    height: 1px;
-    flex-grow: 0;
-    background-color: #555;
-  }
-  .image-23 {
-    width: auto;
-    height: 293.4px;
-    align-self: stretch;
-    flex-grow: 0;
-    border-radius: 12px;
-    background-color: #fff;
-  }
-  header {
-    height: 28px;
-    align-self: stretch;
-    flex-grow: 0;
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: flex-start;
-    gap: 22px;
-    padding: 0;
-  }
-  img.ic_general_like_fill {
-    width: 24px;
-    height: 24px;
-    flex-grow: 0;
-    object-fit: contain;
-  }
+const StArticle = styled.article`
+  margin-bottom: 150px;
 `;
 
 export default Home;
